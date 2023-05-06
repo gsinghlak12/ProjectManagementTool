01ef597c1ecfc28867af45402e92903d5d72dde1
Gurpartap Lakhanpal <gsinghlak12@gmail.com>
2023-05-06 19:48:15 +0100

    Added 'search project by Id' controller, service and repository method

--- PPMTool/src/main/java/com/project/PPMTool/service/ProjectService.java
+++ PPMTool/src/main/java/com/project/PPMTool/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.project.PPMTool.service;
 
-import com.project.PPMTool.exception.ProjectIdException;
+import com.project.PPMTool.exception.BadRequestException;
+import com.project.PPMTool.exception.NotFoundException;
 import com.project.PPMTool.model.Project;
 import com.project.PPMTool.repository.ProjectRepository;
 import java.util.List;
@@ -16,9 +17,17 @@ public class ProjectService {
       project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
       return projectRepository.save(project);
     } catch (Exception e) {
-      throw new ProjectIdException(
+      throw new BadRequestException(
           "Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
     }
+  }
+
+  public Project findByProjectIdentifier(String projectId) {
+    Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+    if (project == null) {
+      throw new NotFoundException("Project ID '" + projectId + "' not found.");
+    }
+    return project;
   }
 
   public List<Project> getProjects() {
