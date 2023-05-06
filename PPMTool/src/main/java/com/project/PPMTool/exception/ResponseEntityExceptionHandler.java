42fe4fd78b56142cacf4cd05d1b734f56ce450e2
Gurpartap Lakhanpal <gsinghlak12@gmail.com>
2023-05-06 20:35:46 +0100

    Modified existing exception handling logic

--- PPMTool/src/main/java/com/project/PPMTool/exception/ResponseEntityExceptionHandler.java
+++ PPMTool/src/main/java/com/project/PPMTool/exception/ResponseEntityExceptionHandler.java
@@ -4,13 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class ResponseEntityExceptionHandler {
   @ExceptionHandler(RuntimeException.class)
-  public final ResponseEntity<Object> handleProjectIdException(
-      BaseException ex) {
+  public final ResponseEntity<Object> handleProjectIdException(BaseException ex) {
     if (ex.getResponseCode() == 400) {
       return new ResponseEntity<>(ex.getErrorMessage(), HttpStatus.BAD_REQUEST);
     } else if (ex.getResponseCode() == 404) {
