8af3b0f1d9711e8c4db19c34e26d870588618110
Gurpartap Lakhanpal <gsinghlak12@gmail.com>
2023-05-20 17:48:34 +0100

    Added router link for CreateProjectButton.js + AddProject component

---
+++ frontend/src/components/Project/AddProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+    render() {
+        return (
+            <>
+                <h1>Add Project Form</h1>
+            </>
+        )
+    }
+}
+
+export default AddProject
