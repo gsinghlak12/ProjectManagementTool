388a64b0441e5a4d1d7b10fd4df5e4769235ea86
Gurpartap Lakhanpal <gsinghlak12@gmail.com>
2023-05-21 11:35:38 +0100

    removed log + cleanup

--- frontend/src/components/Project/AddProject.js
+++ frontend/src/components/Project/AddProject.js
@@ -29,7 +29,6 @@ class AddProject extends Component {
             "start_date": this.state.start_date,
             "end_date": this.state.end_date
         };
-        console.log(newProject)
     }
 
     render() {
