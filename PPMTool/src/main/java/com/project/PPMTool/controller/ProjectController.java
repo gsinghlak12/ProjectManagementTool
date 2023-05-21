4f65be0f479ccabefbf032d31021687dae72111b
Gurpartap Lakhanpal <gsinghlak12@gmail.com>
2023-05-21 14:01:26 +0100

    Added cross-origin support

--- PPMTool/src/main/java/com/project/PPMTool/controller/ProjectController.java
+++ PPMTool/src/main/java/com/project/PPMTool/controller/ProjectController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/projects")
+@CrossOrigin
 public class ProjectController {
 
   @Autowired private ProjectService projectService;
